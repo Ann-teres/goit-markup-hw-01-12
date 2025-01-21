ecd9b5c0b01d9d2f72e6f8b9e6295b0ce6793fde
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-01-21 14:30:56 +0200

    add

--- js/js-hw-01.js
+++ js/js-hw-01.js
@@ -1,1 +1,9 @@
-const categoriesList = document.querySelector
+// Створи дві змінні firstName і lastName.
+// Присвой їм своє ім'я та прізвище.
+// Обєднай ці два рядки в один, з пробілом між ними. Запиши результат в змінну fullName.
+// Виведи fullName в консоль.
+
+
+const firstName = "Anna ";
+const lastName = "Ters";
+console.log (firstName + lastName)
