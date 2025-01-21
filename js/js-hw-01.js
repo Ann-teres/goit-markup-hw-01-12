37ec8993d11d538ea5664dbbfe024cf6f8329647
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-01-21 14:51:52 +0200

    update

--- js/js-hw-01.js
+++ js/js-hw-01.js
@@ -4,6 +4,25 @@
 // Виведи fullName в консоль.
 
 
-const firstName = "Anna ";
+const firstName = "Anna";
 const lastName = "Ters";
-console.log (firstName + lastName)
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Створи дві змінні a і b, присвой їм якісь цілі числа.
+// Виконай над ними додавання, віднімання, множення і ділення.
+// Запиши результати в змінні sum, difference, product, quotient.
+// Виведи всі ці результати в консоль.
+
+let a = 5;
+let b = 8;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+
+console.log("Сума (a + b):", sum);
+console.log("Різниця (a - b):", difference);
+console.log("Добуток (a * b):", product);
+console.log("Частка (a / b):", quotient);
