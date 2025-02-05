08b9218ecea6df164eddea6e8708c93a6c0649d0
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-02-05 13:02:03 +0200

    update

--- js/js-hw-03.js
+++ js/js-hw-03.js
@@ -319,3 +319,371 @@ function replaceSpacesWithHyphens(str) {
         return str.replace(/ /g, '-');
 }
 console.log(replaceSpacesWithHyphens('hello world')); 
+
+// Напиши функцію яка приймає два масиви(arr1, arr2), та повертає новий масив де будуть лише
+//  ті елементи які зустрічаються і в arr1 і arr2.
+
+function getIncomonElement(arr1, arr2) {
+return arr1.filter(element => arr2.includes(element))
+    
+     
+}
+[1, 2, 3, 4], [3, 4, 5, 6]
+console.log(getIncomonElement([1, 2, 3, 4], [3, 4, 5, 6]))
+    
+// Напиши функцію яка приймає два масиви(arr1, arr2), та повертає новий масив де будуть лише елементи масиву arr1 
+// яких не має у масиві arr2.
+
+function differentElem(arr1, arr2) {
+    return arr1.filter(element => !arr2.includes(element))
+} 
+console.log(differentElem([1, 2, 3, 4], [3, 4, 5, 6]))
+
+// Напиши функцію яка приймає масив та знаходить мінімальний елемент.
+
+function findSmallestNumber(arr) {
+    return Math.min(...arr)
+}
+
+console.log(findSmallestNumber([3, 7, 4, 2,]))
+
+// Напиши функцію яка приймає масив та знаходить максимальний елемент.
+
+function findBiggestNumber(arr) {
+    return Math.max(...arr)
+}
+
+console.log(findBiggestNumber([3, 7, 4, 2,]))
+
+// Напиши функцію яка приймає масив та повертає масив лише з тих елементів які більші за 
+// середнє значення всіх елементів масиву.
+
+
+function getElementsAboveAverage(arr) {
+
+      let avg = arr.reduce((a, b) => a + b, 0) / arr.length;
+    return arr.filter(num => num > avg);
+}
+
+console.log(getElementsAboveAverage([1, 4, 2]))
+
+// Напиши функцію, яка приймає масив та виводить кожен елемент масиву у консоль.
+
+function returnElements(arr) {
+    arr.forEach(element => console.log(element))
+}
+returnElements([2, 5, 3])
+
+// Напиши функцію, яка приймає масив та виводить кожен парний елемент масиву у консоль.
+
+function printEvenElements(arr) {
+    arr.forEach(element => {
+        if (element % 2 === 0)
+            console.log(element)
+    })
+}
+
+printEvenElements([4, 2, 3, 4])
+
+// Напиши функцію, яка приймає масив чисел та виводить лише парні числа у консоль.
+
+function giveEvenNumbers(numbers) {
+    numbers.forEach(num => {
+        if (num % 2 === 0) {
+            console.log(num);
+        }
+    });
+}
+
+giveEvenNumbers([10, 15, 22, 33, 40, 55, 60])
+
+// Напиши функцію, яка приймає два параметри(arr, max).Де arr - це масив чисел.Потрібно вивести лише 
+// ті елементи масиву які більші за max.
+
+
+function biggerThanMax(arr, max) {
+    arr.forEach(num => {
+        if (num > max) {
+            console.log(num);
+      }
+  })
+}
+biggerThanMax([4, 2, 6,], 10);
+
+// Напиши функцію яка приймає масив та повертає суму всіх чисел у масиві.
+
+
+// function sumArray(arr) {
+//    return arr.reduce((sum, num) => sum, num, 0)
+// }
+
+// console.log(sumArray([1, 2, 3, 4, 5])); 
+
+// Напиши функцію яка приймає масив та елемент для пошуку.Фукнція повинна порахувати скільки разів шуканий 
+// елемент зустрічається у масиві.
+
+function name(arr, element) {
+    return arr.filter(item => item === element).length;
+}
+
+// Напиши функцію яка приймає масив чисел.Функція повинна видалити усі негативні(відємні числа) з масиву та 
+// повернути новий масив.Напиши функцію яка приймає масив чисел.Функція повинна видалити усі негативні(відємні
+//      числа) з масиву та повернути новий масив.
+
+
+// function removeNegativeNumbers(num) {
+//     return arr.filter(num => num >= 0)
+// }
+// console.log(removeNegativeNumbers([4, -2, 7, -5, 0, -9, 10])); 
+
+// Напиши функцію яка приймає масив рядочків.Потрібно змінити кожен елемент масиву наступним чином -
+//     якщо довжина рядочку менша за 6 до привести рядок до нижнього регістру, в іншому випадку привести 
+//     до верхнього регістру.
+
+// function modifyStrings(str) {
+//   return arr.map(str => {
+//         if (str.length < 6) {
+//             str.toLowerCase
+//         }
+//         else {
+//             str.toUpperCase
+//           }  
+//     })
+// }
+
+// console.log(modifyStrings(["Anna", "Anula", "Anulka", "Kukulka"]))
+
+// Напиши функцію, яка приймає масив чисел і повертає новий масив,
+//     що містить лише непарні числа.
+
+function getOddNumbs(arr,num) {
+    return arr.filter(num => num % 2 !== 0)
+}
+
+console.log(getOddNumbs([4, 8, 6, 9]))
+
+// Напиши функцію, яка приймає масив чисел і повертає новий масив,
+//     що містить лише числа, які діляться на 3.
+
+function thirdNumbers(arr,num) {
+     return arr.filter(num => num % 3 !== 0)
+}
+
+console.log(thirdNumbers([4, 12, 6, 9]))
+
+// Напиши функцію яка приймає два масиви(arr1, arr2),
+//     та повертає новий масив де будуть лише ті елементи які зустрічаються і в arr1 і arr2.
+
+function incommonNumbs(arr1, arr2) {
+    return arr1.filter(element => arr2.includes(element))
+}
+
+
+
+console.log(incommonNumbs( [2, 6, 3, 7], [3, 7, 9, 2]))
+
+// Напиши функцію яка приймає два масиви(arr1, arr2), та повертає новий масив де будуть 
+// лише елементи масиву arr1 яких не має у масиві arr2.
+
+// function findNumersDifference(arr1, arr2) {
+//     const set2 = new Set(arr2);
+//      return arr1.filter(item => !arr2.has(item));
+// }
+
+// console.log(findNumersDifference([1, 6, 2, 8], [5, 2, 7, 0]))
+
+// Напиши функцію яка приймає масив та знаходить мінімальний елемент.
+
+function findMinNumb(arr) {
+    Math.min(...arr)
+}
+
+console.log(findMinNumb([3, 7, 4, 8, 9]));
+
+// Напиши функцію яка приймає масив та знаходить максимальний елемент.
+
+
+function findBiggestNumb(arr) {
+    Math.max(...arr)
+}
+
+console.log(findBiggestNumb([3, 6, 8, 3]));
+
+// Напиши функцію яка приймає масив та повертає масив лише з 
+// тих елементів які більші за середнє значення всіх елементів масиву.
+
+// function getElementsAboveAverage(arr) {
+//   if (arr.length === 0) return [];
+
+//   let arg = arr.reduce((a, b) => a + b, 0) / arr.length
+//     return arr.filter(numb => numb > total)
+// }
+
+// console.log(getElementsAboveAverage([1, 4, 2]))
+
+
+// Напиши функцію яка приймає масив та повертає масив лише
+//  з тих елементів які більші за власний індекс.
+
+// function numbBiggerThenIndex(arr,index) {
+//     return arr.filet((elem, index) => elem > index);
+  
+// }
+
+// console.log(numbBiggerThenIndex([2, 5, 7, 3, 123]));
+
+
+// Напишіть функці яка приймає масив чисел.Функція повинна занйти мінімальний елемент та
+//  видалити його з масиву.
+
+
+
+function removeMinElement(arr) {
+    if (arr.length === 0) return arr; // Перевірка на порожній масив
+    
+    let minIndex = arr.indexOf(Math.min(...arr)); // Знаходимо індекс мінімального елемента
+    return arr.slice(0, minIndex).concat(arr.slice(minIndex + 1)); // Видаляємо мінімальний елемент
+}
+
+// Приклад використання:
+console.log(removeMinElement([5, 2, 8, 3, 10])); // [5, 8, 3, 10]
+console.log(removeMinElement([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
+console.log(removeMinElement([-1, 0, 3, -6, 10])); // [-1, 0, 3, 10]
+console.log(removeMinElement([7])); // []
+console.log(removeMinElement([])); // []
+
+// Напиши функцію, яка приймає масив чисел і значення min.Функція повинна повернути всі елементи, які менші за min.
+
+function filterLessThan(arr, min) {
+    return arr.filter(num => num < min)
+}
+
+console.log(filterLessThan([10, 5, 8, 2, 15, 3], 6)); // [5, 2, 3]
+
+// Напиши функцію, яка приймає масив чисел і повертає їх середнє значення.
+
+
+// function medumNumber(num) {
+//     if (arr.length === 0) return 0;  // Перевірка, щоб уникнути ділення на нуль
+
+//     let sum = arr.reduce((acc, num) => acc + number, 0)  
+//     // Якщо масив порожній, повертається 0, щоб уникнути помилки.
+
+
+//     return sum / arr.length
+// }
+
+// console.log(medumNumber([2,5,3,7,5]))
+// Напиши функцію, яка приймає масив і певний елемент. Функція повинна повернути true, якщо елемент є в масиві, 
+// і false, якщо його немає.
+
+
+
+// Напиши функцію, яка приймає масив чисел і повертає новий масив, у якому відсутні нульові значення.
+
+// function findZeroValue(num) {
+//     const zeroValues = new Set([0, NaN, null, undefined]);
+  
+//        return arr.filter(num => !zeroValues.has(num) && num === num);
+// }
+
+// console.log(findZeroValue([3,0,null, 6,1]))
+
+
+// Напиши функцію, яка приймає масив рядків і повертає новий масив,
+//  у якому всі рядки з парною кількістю символів перетворені на верхній регістр, а з непарною – на нижній.
+
+
+
+function sortByLength(arr) {
+return arr.map(str => {
+        if (str.length % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+// Напиши функцію, яка приймає масив чисел і повертає масив парних чисел.
+
+// function findEven(num) {
+//     return arr.filter(num => num % 2 === 0);
+// }
+// console.log(findEven([4,6,1,7]))
+// Напиши функцію, яка приймає масив чисел і повертає новий масив, де кожен елемент додано до свого індексу.
+
+function addIndex(arr) {
+    return arr.map((num, index) => num + index);
+    
+
+}
+console.log(addIndex(([10, 20, 30, 40])))
+// Напиши функцію, яка приймає масив чисел і повертає новий масив, що містить лише числа, які діляться на 5.
+
+function findDividableOnFive(arr) {
+    return arr.filter(num => num % 5 === 0 )
+}
+console.log(findDividableOnFive([30, 70, 41, 95, 2]))
+
+
+// Напишіть функцію, яка приймає два масиви(arr1, arr2) і повертає новий масив,
+//     що містить елементи з arr2, які відсутні в arr1.
+
+function getNewNum(arr1, arr2) {
+    return arr2.filter(elem => !arr1.includes(elem));
+    }
+
+
+console.log(getNewNum([4, 8, 6], [7, 4, 8]));
+
+// Напишіть функцію, яка приймає масив і повертає його середнє значення
+//     (суму всіх елементів поділену на кількість елементів).
+
+
+function findAvarage(arr) {
+    if (arr.length === 0) return 0;
+    let sum = arr.reduce((acc, num) => acc + num, 0);
+    return sum / arr.length;
+}
+
+console.log(calculateAverage([10, 20, 30, 40]));
+
+// Напишіть функцію, яка приймає масив і повертає масив, що містить лише парні числа з цього масиву.
+
+
+function getEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
+
+// Тестові виклики
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+
+//  Напишіть функцію, яка приймає масив чисел і повертає масив з усіма елементами, кратними числу 3.
+
+function getMultiplesOfThree(arr) {
+    return arr.filter(num => num % 3 === 0);
+}
+
+// Тестові виклики
+console.log(getMultiplesOfThree([3, 6, 9, 12, 15])); // [3, 6, 9, 12, 15]
+
+
+// Напишіть функцію, яка приймає масив і повертає масив,
+//     що містить лише числа, менші за середнє значення всіх елементів масиву.
+
+function lowerThenAvarage(arr) {
+
+    let avarage = arr.reduce((acc, num) => acc + num, 0) /arr.length
+    return arr.filter(num => num < avarage);
+}
+
+console.log(lowerThenAvarage([10, 20, 30, 40]));
+
+// Напишіть функцію, яка приймає масив і повертає новий масив,
+//     в якому кожен елемент буде помножений на його індекс.
+
+function name(arr) {
+    return elem 
+}
+
